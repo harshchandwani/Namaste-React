d69163b1f5b2a94cae912e61b92b2c95e4007c4c
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-09-18 09:03:09 +0530

    Used React Functional Component

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "div",
-  {},
-  React.createElement(
-    "h1",
-    { id: "heading" },
-    "Hey there Harsh Chandwani this side",
-  ),
+const Title = () => (
+
+  <h1> Hey there this is a title</h1>
 );
 
-const jsxHeading = <h1 className="harsh" id="hye">Hello World from JSX</h1>
+const HeadingComponent = () => (
 
-const HeadingComponent = () => {
-  return <h1>Hello world from React Functional Component</h1>
-};
+  <div className="heading" id="container">
+    <h1>Hello world from React Functional Component</h1>
+    <Title></Title>
+
+  </div>
+
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
